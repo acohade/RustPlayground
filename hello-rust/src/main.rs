913036927c262e4d5591e78585a898ac2180ded7
acohade <antoine.cohade@intel.com>
2019-05-19 02:05:07 +0200

    creating debug and build vs code tasks

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,15 +1,7 @@
-extern crate  ferris_says;
-
-
-use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
 fn main() {
-    let stdout = stdout();
-    let out = b"hello fellow humans"; 
-    let width = 24;
 
-
-    let mut writer = BufWriter::new(stdout.lock());
-    say(out,width, &mut writer).unwrap();
+ println!("Hello World!");
+ 
 }
