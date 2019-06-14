3df7e11af208b83081ebc8fa9fcbc760099a2793
acohade <antoine.cohade@intel.com>
2019-06-14 12:22:31 +0200

    rendering image using Vec3 class

--- rust-tracing/src/main.rs
+++ rust-tracing/src/main.rs
@@ -1,24 +1,31 @@
+//std includes
 use std::fs::File;
 use std::io::{self, BufWriter, prelude::*};
+//local includes
+mod vec3;
 
-fn main() -> io::Result<()> {
+fn create_image()-> io::Result<()> {
     let nx = 200; 
     let ny = 100;
     let write_file = File::create("foo.ppm")?;
     let mut writer = BufWriter::new(&write_file);
     write!(&mut writer,"P3\n{} {}\n255\n",nx,ny)?;
 
-    for i in 0..ny {
+    for i in (0..ny).rev() {
         for j in 0..nx {
-            let g = (ny - i) as f32 / ny as f32;
-            let r = j as f32 / nx as f32;
-            let ir = 255.99 * r;
-            let ig = 255.99 * g;
-            let ib = 255.99 * 0.2;
-            write!(&mut writer,"{} {} {} \n", ir as i32, ig as i32, ib as i32)?;
+            let color = vec3::Vec3 { 
+                x: j as f32 / nx as f32,
+                y: i as f32 / ny as f32, 
+                z: 0.2 };
+            let color = 255.99 * color;
+            write!(&mut writer,"{} {} {} \n", color.x as i32, color.y as i32, color.z as i32)?;
         }
         writeln!(&mut writer)?;  
     }
     Ok(())
+}
+
+fn main(){
+    create_image();
 }
 
